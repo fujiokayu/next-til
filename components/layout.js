ec286ed300cc24d79f44329d9e47ce13daf6e2e2
fujiokayu <fujiokayu@gmail.com>
2020-08-29 18:31:05 +0900

    chore

--- components/layout.js
+++ components/layout.js
@@ -3,7 +3,7 @@ import styles from "./layout.module.css"
 import utilStyles from "../styles/utils.module.css"
 import Link from "next/link"
 
-const name = "today I learned NEXT.js"
+const name = "today I learned Next.js"
 export const siteTitle = "Next.js Sample Website"
 
 export default function Layout({ children, home }) {
